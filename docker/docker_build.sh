cb2f8889e6c3d3e3b448ccd6b4ccaaea35fac9e7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-21 11:34:37 +0000

    Adding support for building docker image from 3 repos
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-rm -f devstats.tar 2>/dev/null
-tar cf devstats.tar cmd git metrics docker devel util_sql all lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron vendor util_sh/touch *.go projects.yaml companies.yaml skip_dates.yaml linux.yaml zephyr.yaml github_users.json Makefile
-docker build -t devstats-lfda .
-rm -f devstats.tar
+cwd="`pwd`"
+cd ../devstats || exit 1
+cd ../devstatscode || exit 2
+
+rm -f ../devstats-docker-lf/devstatscode.tar 2>/dev/null
+tar cf ../devstats-docker-lf/devstatscode.tar cmd vendor *.go
+
+cd ../devstats || exit 3
+rm -f ../devstats-docker-lf/devstats.tar 2>/dev/null
+tar cf ../devstats-docker-lf/devstats.tar git metrics devel util_sql all lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron util_sh/touch projects.yaml companies.yaml skip_dates.yaml linux.yaml zephyr.yaml github_users.json Makefile
+
+cd "$cwd" || exit 4
+rm -f devstats-docer-lf.tar 2>/dev/null
+tar cf devstats-docker-lf.tar docker
+
+docker build -f ./docker/Dockerfile -t devstats-lfda .
+rm -f devstats.tar devstatscode.tar devstats-docker-lf.tar
